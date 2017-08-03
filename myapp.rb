3b62be8117fc4bf4bdea936cb0210cb866facded
David Silveira <desenvolvedor.david@gmail.com>
2017-08-03 00:27:23 -0300

    added to site a default layout and created a new folder with css, js and images

--- myapp.rb
+++ myapp.rb
@@ -1,9 +1,24 @@
 require "roda"
  
 class RodaApp < Roda
+	plugin :static, ["/images", "/css", "/js"]
+  plugin :render
+  plugin :head
+
   route do |r|
+  	@more = "more_info"
+
     r.root do
-      "Initial Page"
+      view("index")
     end
+   
+    r.get "drivers_info" do
+    	view("#{@more}/drivers_info")
+    end
+
+    r.get "owner_info" do
+    	view("#{@more}/owner_info")
+    end
+
   end
 end
