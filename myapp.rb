b827a72ada36ad1e860630d3180207d655d9b437
David Silveira <desenvolvedor.david@gmail.com>
2017-08-03 21:13:04 -0300

    Defined a public folder

--- myapp.rb
+++ myapp.rb
@@ -1,26 +1,24 @@
 require "roda"
  
 class RodaApp < Roda
-	plugin :static, ["/images", "/css", "/js"]
+  plugin :static, ["/images", "/css", "/js"], :root=>'public' 
   plugin :render
   plugin :head
 
   route do |r|
-  	@more = "more_info"
+    @more = "more_info"
 
     r.root do
       view("index")
     end
-   
 
     r.get "saiba-mais/condutor" do
-    	view("#{@more}/drivers_info")
+      view("#{@more}/drivers_info")
     end
 
     r.get "saiba-mais/proprietario" do
-    	view("#{@more}/owner_info")
+      view("#{@more}/owner_info")
     end
-
 
   end
 end
